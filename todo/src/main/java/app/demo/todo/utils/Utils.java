5d71ca1fa24af506c9ac9984e25b2061949ba517
jmservera <8036360+jmservera@users.noreply.github.com>
2023-10-30 17:51:30 +0000

    add a new sily method to get a new version

--- todo/src/main/java/app/demo/todo/utils/Utils.java
+++ todo/src/main/java/app/demo/todo/utils/Utils.java
@@ -5,31 +5,48 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Utils {
-    private static DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm a z");
+  private static DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm a z");
 
-    public static String toJsonValueContent(String value) {
-        if (value == null)
-          return "null";
-        return value.replace("\'", "\\'").replace("\"", "\\\"");
-    }
-    
-    public static String toJsonValueContent(Date value) {
-        if (value == null)
-          return "null";
-        return DATE_FORMAT.format(value);
+  public static String toJsonValueContent(String value) {
+    if (value == null)
+      return "null";
+    return value.replace("\'", "\\'").replace("\"", "\\\"");
+  }
+
+  public static String toJsonValueContent(Date value) {
+    if (value == null)
+      return "null";
+    return DATE_FORMAT.format(value);
+  }
+
+  public static String shortenString(String value) {
+
+    return shortenString(value, 7);
+    // if (value == null) {
+    // return "";
+    // }
+
+    // if (value.length() <= 5) {
+    // return value;
+    // }
+
+    // return value.substring(0, 4) + "...";
+  }
+
+  public static String shortenString(String value, int maxLength) {
+
+    if (value == null) {
+      return "";
     }
 
-    public static String shortenString(String value) {
-			
-      if (value == null) {
-        return "";
-      }
-      
-      if (value.length() <= 5)
-      {
-        return value;
-      }
-
-      return value.substring(0, 4) + "...";
+    if (maxLength < 7) {
+      maxLength = 7;
     }
+
+    if (value.length() <= maxLength) {
+      return value;
+    }
+
+    return value.substring(0, maxLength - 3) + "...";
+  }
 }
